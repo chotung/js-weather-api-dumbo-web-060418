57905822840bff4f7d6da3cefb80b5cbd4e2ec57
mark sauer-utley <mark.utley.713@gmail.com>
2018-07-19 18:12:25 -0400

    update vars to const and let

--- js/init.js
+++ js/init.js
@@ -1,9 +1,9 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-  var API_KEY = "0525fcf0352eb6b3ae9200927f0a8e2f";
-  var CORS_WRAPPER = "https://cors-anywhere.herokuapp.com/"
-  var URL = CORS_WRAPPER + "https://api.darksky.net/forecast/" + API_KEY + "/40.7127,-74.0059?exclude=currently?exclude=minutely?exclude=daily'";
-  var ctx = document.getElementById('NYCWeatherChart').getContext('2d');
+  const API_KEY = "0525fcf0352eb6b3ae9200927f0a8e2f";
+  const CORS_WRAPPER = "https://cors-anywhere.herokuapp.com/"
+  const URL = CORS_WRAPPER + "https://api.darksky.net/forecast/" + API_KEY + "/40.7127,-74.0059?exclude=currently?exclude=minutely?exclude=daily'";
+  const ctx = document.getElementById('NYCWeatherChart').getContext('2d');
 
   makeRequest(URL, function(json) {
     let labels = getHourSet(json['hourly']['data'])
